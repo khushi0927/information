a3ca19aaaad32a3a0fb769f74108c3730aae0e9c
khushi0927 <kp6999736@gmail.com>
2025-11-12 11:18:30 +0530

    commit

--- Admin/class/User.php
+++ Admin/class/User.php
@@ -23,7 +23,7 @@ class User {
     }
 
      
-    public function login($Username, $Password) {
+    public function login($Email, $Password) {
         $Email = $this->conn->real_escape_string($Email);
         $query = "SELECT * FROM {$this->table} WHERE Email='$Email'";
         $result = $this->conn->query($query);
